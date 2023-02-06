c3275480b5fc49f4e57bb82e9e1848ed1411a82a
Sara-Cb <saramaiden23@gmail.com>
2023-02-06 16:48:08 +0100

    ok

--- Week_2/Day_1/assets/js/index.js
+++ Week_2/Day_1/assets/js/index.js
@@ -3,5 +3,5 @@ window.alert('Sto studiando JS');
 
 var autore = "scritto da Javascript"
 
-document.getElementById('title').innerHTML = "Iniziamo la nostra pratica"
+document.getElementById('title').innerHTML = "Iniziamo la nostra pratica" //oppure document.write=("<h1>Iniziamo la nostra pratica</h1>")
 document.getElementById('output').innerHTML = ("output " + autore)
