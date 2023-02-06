4f7b106a223ec98ffaa8eb2fb026f63db127381b
Sara-Cb <saramaiden23@gmail.com>
2023-02-06 16:31:46 +0100

    Esercizio 1 JS

--- Week_2/Day_1/assets/js/index.js
+++ Week_2/Day_1/assets/js/index.js
@@ -1,2 +1,7 @@
-alert('Usa il pop up per dare dei messaggi');
+alert('Ciao, sono Sara');
 window.alert('Sto studiando JS');
+
+var autore = "scritto da Javascript"
+
+document.getElementById('title').innerHTML = "Iniziamo la nostra pratica"
+document.getElementById('output').innerHTML = ("output " + autore)
--- Week_2/Day_1/assets/js/naming.js
+++ Week_2/Day_1/assets/js/naming.js
@@ -1,18 +1,10 @@
-//var 1 pet_preferito = 'gatto';
-//console.log(1 pet_preferito);
-
-/*var pet = 'gatto';
-var Pet = 'cane';*/
-var petPreferito = 'criceto';
-var PetPreferito = 'coniglio';
-
 var petCriceto = 'criceto';
-console.log(petCriceto);
 var pet_giraffa = 'giraffa';
-console.log(pet_giraffa);
 var petleone = 'leone';
-console.log(petleone);
-var _pet2 = 'pantera';
-document.write(_pet2);
+var PetPantera = 'pantera';
 
-//var 12 = 'numero';
+console.log(petCriceto);
+console.log(pet_giraffa);
+console.log(petleone);
+console.log(PetPantera);
+document.write(PetPantera + ", scritto da Javascript")
