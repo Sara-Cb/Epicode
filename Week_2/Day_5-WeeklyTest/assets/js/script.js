b5504946c236114ce1b304d3d2f9314a0282f03b
Sara-Cb <saramaiden23@gmail.com>
2023-02-10 16:31:41 +0100

    Weekly

--- Week_2/Day_5-WeeklyTest/assets/js/script.js
+++ Week_2/Day_5-WeeklyTest/assets/js/script.js
@@ -1,9 +1,11 @@
 let stringa = document.getElementById("display");
 
 
-function display(value){
-    stringe.innerHTML += value};
+function display(value){stringa.value += value};
 
 //all clear function
 function AC() {stringa.value = " "};
 
+function equal(){
+    stringa.value = eval(stringa.value)
+}
