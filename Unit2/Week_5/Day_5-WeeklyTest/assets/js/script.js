4945ca4d3aa38cbb4d67474e04fbd345ed54fd9e
Sara-Cb <saramaiden23@gmail.com>
2023-03-06 00:44:15 +0100

    animazione!

--- Unit2/Week_5/Day_5-WeeklyTest/assets/js/script.js
+++ Unit2/Week_5/Day_5-WeeklyTest/assets/js/script.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('header');
 const hero = document.getElementById('hero');
 const btnHeader = document.querySelector('header button')
+const groups = document.querySelectorAll('svg g g g');
+
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > hero.offsetTop + hero.offsetHeight-75) {
@@ -11,3 +13,28 @@ window.addEventListener('scroll', function() {
       btnHeader.classList.remove('green');
     }
   });
+
+  function randomGroup() {
+    const remainingGroups = [...groups].filter(group => !group.used);
+    const randomIndex = Math.floor(Math.random() * remainingGroups.length);
+    const group = remainingGroups[randomIndex];
+    group.used = true;
+    return group;
+  }
+  
+  function animateGroup(group) {
+    let opacity = 1;
+    const intervalId = setInterval(function() {
+      opacity = opacity === 1 ? 0 : 1;
+      group.setAttribute('opacity', opacity);
+      if (opacity === 1) {
+        group.used = false;
+        clearInterval(intervalId);
+      }
+    }, 500);
+  }
+ 
+setInterval(function() {
+  const group = randomGroup();
+  animateGroup(group);
+}, 100);
