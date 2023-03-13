1c01be40434d28cb2c377235a2e794f459062acb
Sara-Cb <saramaiden23@gmail.com>
2023-03-13 16:54:37 +0100

    double

--- Unit2/Week_7/Day_1/assets/js/script.js
+++ Unit2/Week_7/Day_1/assets/js/script.js
@@ -21,9 +21,42 @@ class User {
   let user2 = new User("Francesca", "Verdi", 25, "Roma");
   let user3 = new User("Filippo", "Bianchi", 27, "Firenze");
 
-  console.log(user1.compareAge(user2));
 
-
+  const userForm = document.getElementById("users-form");
+  const userList = document.getElementById("listUsers");
+  
+  userForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+    const ageInput = document.getElementById("age");
+    const locationInput = document.getElementById("location");
+    
+    const newUser = new User(
+      firstNameInput.value,
+      lastNameInput.value,
+      ageInput.value,
+      locationInput.value
+    );
+    
+    // Aggiungo alla lista
+    const li = document.createElement("li");
+    li.textContent = `${newUser.firstName} ${newUser.lastName}, ${newUser.age}) - from ${newUser.location}`;
+    userList.appendChild(li);
+  
+    // Aggiungo delete
+    const delBtn = document.createElement('button');
+    delBtn.className = 'delBtn';
+    delBtn.innerHTML = 'Delete';
+    li.appendChild(delBtn);
+  
+      delBtn.addEventListener('click', function () {
+          userList.removeChild(li);
+      })
+    
+    userForm.reset();
+  });
 
 //es.2
 
