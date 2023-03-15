529f14b90ac1264634e3b72be59cd74e06cff066
Sara-Cb <saramaiden23@gmail.com>
2023-03-15 17:46:39 +0100

    sistemata estetica

--- Unit2/Week_7/Day_3/assets/js/index.js
+++ Unit2/Week_7/Day_3/assets/js/index.js
@@ -11,8 +11,11 @@ cartDelBtn.addEventListener("click", () => {
 });
 
 function createCard(fetchData) {
-  let newCard = document.createElement("article");
-  newCard.setAttribute("class", "card");
+  let newCard = document.createElement("div");
+  newCard.setAttribute("class", "col");
+
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
 
   let cardImgDiv = document.createElement("div");
   cardImgDiv.setAttribute("class", "cardImgDiv");
@@ -22,15 +25,19 @@ function createCard(fetchData) {
   cardImg.setAttribute("src", fetchData.img);
 
   let cardBody = document.createElement("div");
-  cardBody.setAttribute("class", "card-body");
+  cardBody.setAttribute("class", "card-body d-flex flex-column justify-content-around");
 
-  let cardTitle = document.createElement("div");
+  let cardTextDiv = document.createElement("div");
+
+  let cardTitle = document.createElement("h4");
   cardTitle.setAttribute("class", "card-title");
   cardTitle.innerText = fetchData.title;
 
-  let cardText = document.createElement("div");
+  let cardText = document.createElement("p");
   cardText.setAttribute("class", "card-text");
   cardText.innerText = fetchData.price + " €";
+
+  let cardBtnsDiv = document.createElement("div");
 
   let newcardDelBtn = document.createElement("button");
   newcardDelBtn.setAttribute("class", "btn");
@@ -63,28 +70,37 @@ function createCard(fetchData) {
     cartReference.appendChild(bookinCartText);
   });
 
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(cardText);
-  cardBody.appendChild(newcardDelBtn);
-  cardBody.appendChild(addToCart);
+  cardBody.appendChild(cardTextDiv);
+  cardBody.appendChild(cardBtnsDiv);
+  
+  cardTextDiv.appendChild(cardTitle);
+  cardTextDiv.appendChild(cardText);
+
+  cardBtnsDiv.appendChild(newcardDelBtn);
+  cardBtnsDiv.appendChild(addToCart);
 
   cardImgDiv.appendChild(cardImg);
 
-  newCard.appendChild(cardImgDiv);
-  newCard.appendChild(cardBody);
+  card.appendChild(cardImgDiv);
+  card.appendChild(cardBody);
+
+  newCard.appendChild(card);
 
   booksReference.appendChild(newCard);
 }
 
 fetch("https://striveschool-api.herokuapp.com/books")
+
   .then((response) => {
     return response.json();
   })
+
   .then((payload) => {
     payload.forEach((book) => {
       createCard(book);
     });
   })
+
   .catch((err) => {
     console.log(err);
   });
