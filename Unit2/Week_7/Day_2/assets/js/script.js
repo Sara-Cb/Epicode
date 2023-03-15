ce4d8768c320666fbad4c1147e5603f1fbd0dc7f
Sara-Cb <saramaiden23@gmail.com>
2023-03-15 10:24:58 +0100

    functioning

---
+++ Unit2/Week_7/Day_2/assets/js/script.js
@@ -0,0 +1,49 @@
+const listReference = document.getElementById("userList");
+const formReference = document.getElementById("userForm");
+const delBtn = document.getElementById("delBtn");
+const nameReference = document.getElementById("name").value;
+const timer = document.getElementById("timer");
+let nameIndex = 0;
+
+
+if (!sessionStorage.getItem("time")) {
+  sessionStorage.setItem("time", 0);
+}
+
+window.addEventListener("load", () => {
+  for (i = 0; i < localStorage.length; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = localStorage.getItem(`Name-${i}`);
+    listReference.appendChild(newLi);
+  }
+
+  let d = sessionStorage.getItem("time");
+  let time = parseInt(d);
+
+  setInterval(() => {
+    time += 1;
+    sessionStorage.setItem("time", time);
+    timer.innerText = sessionStorage.getItem("time");
+  }, 1000);
+});
+
+formReference.addEventListener("submit", function (e) {
+  e.preventDefault();
+  localStorage.setItem(`Name-${nameIndex}`, nameReference);
+  let newLi = document.createElement("li");
+  newLi.innerText = localStorage.getItem(`Name-${nameIndex}`);
+  listReference.appendChild(newLi);
+  nameIndex += 1;
+  nameReference = "";
+});
+
+formReference.addEventListener("reset", () => {
+  listReference.innerText = "";
+  localStorage.clear();
+});
+
+delBtn.addEventListener("click", function () {
+  nameIndex -= 1;
+  localStorage.removeItem(`Name-${nameIndex}`);
+  listReference.removeChild(listReference.lastChild);
+});
