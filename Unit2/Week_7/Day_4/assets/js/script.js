f1a71798f3cf22f10f404625b0603c1943f40813
Sara-Cb <saramaiden23@gmail.com>
2023-03-16 16:47:08 +0100

    wip (male)

--- Unit2/Week_7/Day_4/assets/js/script.js
+++ Unit2/Week_7/Day_4/assets/js/script.js
@@ -1,8 +1,8 @@
 const myKey = "[3WxA53hU3FVfycS4IbCuYjIAvGxa84tk008mZBK7JbxkW0BboLyVvssj]";
 const FRONT_PHOTOS = "https://api.pexels.com/v1/search?query=Sea";
 const BACK_PHOTOS = "https://api.pexels.com/v1/search?query=Mountain";
-const loadFrontBtn = document.getElementById("#loadFrontBtn");
-const loadBackBtn = document.getElementById("#loadBackBtn");
+const loadFrontBtn = document.getElementById("loadBtn");
+const loadBackBtn = document.getElementById("loadBackBtn");
 
 const showPics = function () {};
 
@@ -10,8 +10,10 @@ const getFrontPhotos = function () {
   fetch(FRONT_PHOTOS, {
     method: "GET",
     headers: {
-      Authorization: myKey,
+      Authorization:
+        "[3WxA53hU3FVfycS4IbCuYjIAvGxa84tk008mZBK7JbxkW0BboLyVvssj]",
     },
+    mode: "no-cors",
   })
     .then((response) => response.json())
     .then((data) => {
@@ -22,28 +24,27 @@ const getFrontPhotos = function () {
     });
 };
 
-const getBackPhotos = function () {
-  fetch(BACK_PHOTOS, {
-    method: "GET",
-    headers: {
-      Authorization: myKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      showPics(data.photos);
-    })
-    .catch((error) => {
-      console.error("Something went wrong", error);
+const getBackPhotos = async function () {
+  try {
+    const response = await fetch(BACK_PHOTOS, {
+      method: "GET",
+      headers: {
+        Authorization: myKey,
+      },
     });
+    const data = await response.json();
+    showPics(data.photos);
+  } catch (error) {
+    console.error("Something went wrong", error);
+  }
 };
 
-loadFrontBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+loadFrontBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   getFrontPhotos();
 });
 
-loadBackBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+loadBackBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   getBackPhotos();
 });
