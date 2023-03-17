8e92e64ddece51cd149f1272680051873b5de6e7
Sara-Cb <saramaiden23@gmail.com>
2023-03-17 14:49:01 +0100

    better

--- Unit2/Week_7/Day_5-Weekly_test/assets/js/script.js
+++ Unit2/Week_7/Day_5-Weekly_test/assets/js/script.js
@@ -3,6 +3,44 @@ const myKey =
 const myEndpoint = "https://striveschool-api.herokuapp.com/api/product/";
 const rowReference = document.getElementById("bestSellers");
 
+const createCards = (products) => {
+  rowReference.innerHTML = ``;
+  products.forEach((product, index) => {
+    const newCol = document.createElement("div");
+    newCol.setAttribute("class", "col-4 col-md-3 col-lg-2");
+    newCol.setAttribute("id", index);
+
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    const cardImg = document.createElement("div");
+    cardImg.classList.add("card-img-top");
+    cardImg.innerHTML = `<img src=${product.imageUrl} alt='product image' class='prodImg'>`;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    cardBody.innerHTML = `<div>
+      <h3 class="card-title">${product.name}</h3>
+      <h4 class="card-subtitle">${product.brand}</h4>
+  </div>
+  <div class="card-text">
+      <p class='cardDescription'>${product.description}</p>
+      <br>
+      <hr>
+      <p class='cardPrice'>${product.price}€</p>
+  </div>
+  <div>
+      <a class="btn" href="${myEndpoint + product._id}">Details</a>
+  </div>`;
+
+    newCard.appendChild(cardImg);
+    newCard.appendChild(cardBody);
+
+    newCol.appendChild(newCard);
+    rowReference.appendChild(newCol);
+  });
+};
+
 const getProducts = async function () {
   try {
     let response = await fetch(myEndpoint, {
@@ -14,8 +52,9 @@ const getProducts = async function () {
     console.log(response);
     if (response.ok) {
       let respJson = await response.json();
-      let products = respJson.product;
-      console.log(products);
+      console.log(respJson);
+      let products = Array.from(respJson);
+      createCards(products);
       return products;
     } else {
       console.log("PROBLEM");
@@ -26,4 +65,4 @@ const getProducts = async function () {
   }
 };
 
-getProducts(myEndpoint);
+getProducts();
