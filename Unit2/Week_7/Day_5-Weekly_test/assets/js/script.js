dd2021784dcd8634ccdfabb247b0d8f526022c25
Sara-Cb <saramaiden23@gmail.com>
2023-03-17 13:57:43 +0100

    quasi capendo

--- Unit2/Week_7/Day_5-Weekly_test/assets/js/script.js
+++ Unit2/Week_7/Day_5-Weekly_test/assets/js/script.js
@@ -1,13 +1,14 @@
 const myKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEzY2IyY2M1NmIzNjAwMTMzZmU1N2QiLCJpYXQiOjE2NzkwNDkxOTksImV4cCI6MTY4MDI1ODc5OX0.rO07M97K2A2Flfk9JOsNIzw8pQa2wRIVQU0a356TdxI";
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEzY2IyY2M1NmIzNjAwMTMzZmU1N2QiLCJpYXQiOjE2NzkwNDkxOTksImV4cCI6MTY4MDI1ODc5OX0.rO07M97K2A2Flfk9JOsNIzw8pQa2wRIVQU0a356TdxI";
 const myEndpoint = "https://striveschool-api.herokuapp.com/api/product/";
+const rowReference = document.getElementById("bestSellers");
 
-const getProducts = async (url) => {
+const getProducts = async function () {
   try {
-    let response = await fetch(url, {
+    let response = await fetch(myEndpoint, {
+      method: "GET",
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEzY2IyY2M1NmIzNjAwMTMzZmU1N2QiLCJpYXQiOjE2NzkwNDkxOTksImV4cCI6MTY4MDI1ODc5OX0.rO07M97K2A2Flfk9JOsNIzw8pQa2wRIVQU0a356TdxI",
+        Authorization: myKey,
       },
     });
     console.log(response);
